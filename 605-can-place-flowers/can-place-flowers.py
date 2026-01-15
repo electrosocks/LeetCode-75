0534a739e820dc555536a6b0a0f69ec053c01b1f
electrosocks <electrosocks5@gmail.com>
2026-01-15 11:50:13 -0600

    Time: 11 ms (39.70%) | Memory: 19.7 MB (8.41%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -6,11 +6,13 @@ class Solution:
         
         # Check all Pots
         for i in range(len(flowerbed)):
+
             # Check if ith pot, i-1th pot, and i+1th pot are empty
             if flowerbed[i] == 0:
-                empty_pot_left = (i == 0) or flowerbed[i - 1] == 0
-                empty_pot_right = (i == len(flowerbed) - 1)  or flowerbed[i + 1] == 0
+                empty_pot_left = (i == 0) or flowerbed[i - 1] == 0                          # Checks left pot and edge case
+                empty_pot_right = (i == len(flowerbed) - 1)  or flowerbed[i + 1] == 0       # Checks right pot and edge case
 
+                # If empty, add to count
                 if empty_pot_right and empty_pot_left:
                     sum += 1                # Add to count
                     flowerbed[i] = 1        # Add plant (to ensure we account for new potted plant)
