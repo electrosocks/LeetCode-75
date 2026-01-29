a29fa47be18823abe594af819a502b4c15fe72d8
electrosocks <electrosocks5@gmail.com>
2026-01-29 16:18:50 -0600

    Time: 43 ms (94.32%) | Memory: 22.3 MB (10.99%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -2,25 +2,33 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         
         # Define variables
-        maxNum = 0
-        length = len(nums)
-        tally = 0
         lp = 0
-        rp = 0
+        zerocount = 0
+        length = len(nums)
+        maxVal = 0
 
+        # Main loop
         for i in range(0, length):
+            
+            # Check if value in loop is zero, add to count
             if nums[i] == 0:
-                tally += 1
-            if tally > k:
+                zerocount += 1
+            
+            # Check if zerocount is greater than k
+            if zerocount > k:
+
+                # If it is, check if left most value getting left behind is zero and decrease count
                 if nums[lp] == 0:
-                    tally -= 1
+                    zerocount -= 1
+
+                # Move window right
                 lp += 1
+            
+            # Update if value is higher
+            if (i - lp + 1) > maxVal:
+                maxVal = i - lp + 1
 
-
-            if (i - lp + 1) > maxNum:
-                maxNum = i - lp + 1
-        
-        return maxNum
-
+        # Return result
+        return maxVal
             
 
